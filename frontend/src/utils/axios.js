aa58263122fde120b7122c40eb23e2a2f6ece3d4
allanwono <allan.wono@gmail.com>
2026-01-05 12:55:46 +0530

    gitignore

---
+++ frontend/src/utils/axios.js
@@ -0,0 +1,28 @@
+// src/utils/axios.js
+import axios from "axios";
+
+// const axiosPrivate = axios.create({
+//   baseURL: import.meta.env.VITE_API_BASE_URL,
+//   withCredentials: true,
+// });
+const x = 2;
+
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_DEV_LINK,
+});
+
+export const axiosPrivate = axios.create({
+  baseURL: import.meta.env.VITE_DEV_LINK,
+  withCredentials: true,
+});
+
+// export const api = axios.create({
+//   baseURL: import.meta.env.VITE_PROD_LINK,
+// });
+
+// export const axiosPrivate = axios.create({
+//   baseURL: import.meta.env.VITE_PROD_LINK,
+//   withCredentials: true,
+// });
+
+export default axiosPrivate;
