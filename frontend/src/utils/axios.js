4e8dda29473a83bca75d99a75d123c39bfcc1744
allanwono <allan.wono@gmail.com>
2026-01-07 14:39:40 +0530

    axios dev

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -7,22 +7,22 @@ import axios from "axios";
 // });
 const x = 2;
 
-// export const api = axios.create({
-//   baseURL: import.meta.env.VITE_DEV_LINK,
-// });
-
-// export const axiosPrivate = axios.create({
-//   baseURL: import.meta.env.VITE_DEV_LINK,
-//   withCredentials: true,
-// });
-
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_PROD_LINK,
+  baseURL: import.meta.env.VITE_DEV_LINK,
 });
 
 export const axiosPrivate = axios.create({
-  baseURL: import.meta.env.VITE_PROD_LINK,
+  baseURL: import.meta.env.VITE_DEV_LINK,
   withCredentials: true,
 });
+
+// export const api = axios.create({
+//   baseURL: import.meta.env.VITE_PROD_LINK,
+// });
+
+// export const axiosPrivate = axios.create({
+//   baseURL: import.meta.env.VITE_PROD_LINK,
+//   withCredentials: true,
+// });
 
 export default axiosPrivate;
